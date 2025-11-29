5737e8381826eb3a67150314649a0159d2bf9f37
Gabriel <gabb.fz25@gmail.com>
2025-11-29 17:03:11 -0300

    cuarto commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
@@ -23,7 +23,8 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('about_us/', views.about_us, name='about_us')
+    path('about_us/', views.about_us, name='about_us'),
+    path('blog/', include('posts.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from . models import Post, Categoria
 
 # Create your views here.
+def post_list(request):
+    id_categoria = request.GET.get('id')
+
+    if id_categoria:
+        posts = Post.objects.filter(categoria_post=id_categoria)
+    else:
+        posts = Post.objects.all()
+
+    categorias = Categoria.objects.all().order_by('nombre')
+
+    context = {
+        'posts': posts,
+        'categoria': categorias
+    }
+
+    return render(request, 'posts/post_list.html', context)
+
+
+
